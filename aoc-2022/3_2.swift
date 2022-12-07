5c352787bb658d372d70ab89d8d0aaa5c7c890a8
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-07 16:27:38 +0100

    3_2

---
+++ aoc-2022/3_2.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
+let lines = input.split(separator: "\n", omittingEmptySubsequences: false)
+
+func char_to_priority(c: Character) -> UInt8 {
+    switch (c) {
+    case "a"..."z": return (c.asciiValue ?? 0) - 96
+    case "A"..."Z": return (c.asciiValue ?? 0) - 64 + 26
+    default: 
+        assert(false)
+    }
+}
+var sum : UInt = 0
+for i in stride(from: 0, to: lines.count, by: 3) {
+  if lines[i] == "\n" || lines[i].isEmpty { break }
+
+  let x = Set(lines[i]), y = Set(lines[i+1]), z = Set(lines[i+2])
+
+
+  let inter =  x.intersection(y).intersection(z)
+  precondition(inter.count >= 1, "\(inter) \(x) \(y) \(z)")
+
+  let c = inter.first!
+  let priority = char_to_priority(c: c)
+
+  //print(line, left, right, inter, priority)
+
+  sum += UInt(priority)
+}
+
+print(sum)
