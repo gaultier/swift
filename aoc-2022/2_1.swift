c684e1f6afa2cfa4c84e2bb6b5a1a37d5e36dd20
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-07 14:28:16 +0100

    simplify

--- aoc-2022/2_1.swift
+++ aoc-2022/2_1.swift
@@ -4,9 +4,9 @@ let input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: Stri
 let lines = input.split(separator: "\n", omittingEmptySubsequences: false)
 
 enum Play: UInt {
-    case Rock = 0, Paper = 1, Scissor = 2
+    case Rock = 1, Paper = 2, Scissor = 3
     func score() -> UInt {
-        return 1 + rawValue
+        return rawValue
     }
 }
 
@@ -15,17 +15,29 @@ for line in lines {
     let plays = line.split(separator: " ")
     if (plays.isEmpty) { break }
     
-    let left = UInt((plays[0].first?.asciiValue ?? 0) - (Character("A").asciiValue ?? 0))
-    let their_play = Play(rawValue: left)!
+    var their_play : Play = .Rock
+    switch(plays[0]) {
+        case "A": their_play = .Rock
+        case "B": their_play = .Paper
+        case "C": their_play = .Scissor
+        default:
+            precondition(false, "Expected A|B|C, got: \(plays[0])")
+    }
 
-    let right = UInt((plays[1].first?.asciiValue ?? 0) - (Character("X").asciiValue ?? 0))
-    let my_play = Play(rawValue: right)!
+    var my_play : Play = .Rock
+    switch(plays[1]) {
+        case "X": my_play = .Rock
+        case "Y": my_play = .Paper
+        case "Z": my_play = .Scissor
+        default:
+            precondition(false, "Expected X|Y|Z, got: \(plays[0])")
+    }
 
     var play_score: UInt = 0 
     switch ((their_play, my_play)) {
         case (_, _) where their_play == my_play: play_score = 3 
         case (_, _) where my_play.rawValue > their_play.rawValue: play_score = 6
-        case (_, _):  do{}
+        default:  do{}
     }
     let round_score = my_play.score() + play_score
     print(their_play, my_play, "\(my_play.score()) + \(play_score) = \(round_score) => total before = \(total_score), after = \(total_score+round_score)")
