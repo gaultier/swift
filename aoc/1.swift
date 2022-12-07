e5c1afaeabc62ea6f8f4ce63959bc7eaad94c988
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-07 10:52:17 +0100

    first

---
+++ aoc/1.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
+let lines = input.split(separator: "\n", omittingEmptySubsequences: false)
+
+var elves : [UInt] = [0] // Start with one elf
+elves.reserveCapacity(10_000)
+
+for line in lines {
+    if line.isEmpty {
+        elves.append(0)
+    } else {
+        let num = UInt(line) ?? 0
+        elves[elves.count - 1 ] += num
+    }
+}
+
+print(elves.max() ?? 0)
