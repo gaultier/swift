320c764030e8c10593524c9aef54c05526dd4f86
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-08 12:54:38 +0100

    wip 5_1

--- aoc-2022/5_1.swift
+++ aoc-2022/5_1.swift
@@ -21,7 +21,13 @@ for (row, line) in lines.enumerated() {
 
             remaining = remaining[crate_end...].dropFirst()
         }
-    } else { // Moves
-        print(row, line)
+    } else if (line.starts(with: "move")) { // Moves
+        let parts = line.split(separator: " ")
+        precondition(parts.count == 6, parts.description)
+
+        let crates_count = UInt(parts[1]) ?? 0
+        let from = UInt(parts[3]) ?? 0
+        let to = UInt(parts[5]) ?? 0
+        print(row, crates_count, from, to)
     }
 }
