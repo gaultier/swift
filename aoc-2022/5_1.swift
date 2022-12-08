31d57d28a79bf402c20a96b8a8f300b1d0ac0606
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-08 14:21:17 +0100

    5_1 finish

--- aoc-2022/5_1.swift
+++ aoc-2022/5_1.swift
@@ -1,9 +1,16 @@
 import Foundation
 
+struct Move {
+    let count: Int
+    let from: Int
+    let to: Int
+}
+
 var input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
 let lines = input.split(separator: "\n")
 
 var stacks : Array<Array<Character>> = [[]]
+var moves : Array<Move> = []
 
 for (row, line) in lines.enumerated() {
     if (line.isEmpty) { break }
@@ -16,17 +23,17 @@ for (row, line) in lines.enumerated() {
             let crate_begin = remaining.firstIndex(of: "[")
             if (crate_begin == nil) { break }
 
-            let crate_end = remaining[crate_begin.unsafelyUnwrapped...].firstIndex(of: "]").unsafelyUnwrapped
-            let crate = remaining[crate_begin.unsafelyUnwrapped..<crate_end]
+            let crate_end = remaining[crate_begin!...].firstIndex(of: "]")!
+            let crate = remaining[crate_begin!..<crate_end]
 
             let crate_name = crate.dropFirst()
-            let column = line[...crate_begin.unsafelyUnwrapped].count / 4
+            let column = line[...crate_begin!].count / 4
             print(row, column, crate_name)
 
             if (stacks.count <= column) {
                 stacks.append([])
             }
-            stacks[column].insert(crate_name.first.unsafelyUnwrapped, at: 0)
+            stacks[column].insert(crate_name.first!, at: 0)
 
             remaining = remaining[crate_end...].dropFirst()
         }
@@ -34,11 +41,23 @@ for (row, line) in lines.enumerated() {
         let parts = line.split(separator: " ")
         precondition(parts.count == 6, parts.description)
 
-        let crates_count = UInt(parts[1]) ?? 0
-        let from = UInt(parts[3]) ?? 0
-        let to = UInt(parts[5]) ?? 0
+        let crates_count = Int(parts[1]) ?? 0
+        // 1-indexed
+        let from = (Int(parts[3]) ?? 0) - 1
+        let to = (Int(parts[5]) ?? 0) - 1
         print(row, crates_count, from, to)
+
+        moves.append(Move(count: crates_count, from: from, to: to))
     }
 }
 
-print(stacks)
+for move in moves {
+    for _ in 1...move.count {
+        let top = stacks[move.from].last!
+        stacks[move.to].append(top)
+        stacks[move.from] = stacks[move.from].dropLast()
+    }
+}
+
+let tops = stacks.map({ st in String(st.last!) }).joined()
+print(tops)
