7289f723048ffb75818141651ac390b37ae83136
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-08 12:47:50 +0100

    rename

--- aoc-2022/5_1.swift
+++ aoc-2022/5_1.swift
@@ -3,19 +3,19 @@ import Foundation
 var input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
 let lines = input.split(separator: "\n", omittingEmptySubsequences: false)
 
-for (i, line) in lines.enumerated() {
+for (row, line) in lines.enumerated() {
     if (line == "\n" || line.isEmpty) { break }
 
     var remaining = line
 
-    while (true) {
+    for column in  0... {
         let crate_begin = remaining.firstIndex(of: "[")
         if (crate_begin == nil) { break }
 
         let crate_end = remaining[crate_begin.unsafelyUnwrapped...].firstIndex(of: "]").unsafelyUnwrapped
         let crate = remaining[crate_begin.unsafelyUnwrapped..<crate_end]
 
-        print(i, crate.dropFirst())
+        print(row, column, crate.dropFirst())
 
         remaining = remaining[crate_end...].dropFirst()
     }
