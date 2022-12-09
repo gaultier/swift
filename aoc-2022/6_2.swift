0ac0fcacff54c144ce443576c0a54740c363c191
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-09 11:02:38 +0100

    6_2

--- aoc-2022/6_2.swift
+++ aoc-2022/6_2.swift
@@ -3,12 +3,13 @@ import Foundation
 
 var input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
 
-for i in 1...(input.count - 4) {
-    let a = input.prefix(i + 0).last!
-    let b = input.prefix(i + 1).last!
-    let c = input.prefix(i + 2).last!
-    let d = input.prefix(i + 3).last!
-    if (a == b || a == c || a == d || b == c || b == d || c == d) { continue }
-    print(i+3, a, b, c, d)
-    break
+for i in 1...(input.count - 14) {
+    let index = input.index(input.startIndex, offsetBy: i)
+    let msg = input[index...].prefix(14)
+    let chars :Set<UInt8> = Set(msg.utf8)
+
+    if (msg.count == chars.count) {
+        print(i + 14)
+        break
+    }
 }
