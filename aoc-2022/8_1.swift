ef832220189b650f5cf4127905479670ee7496c1
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-12 10:07:21 +0100

    8_1

---
+++ aoc-2022/8_1.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+let input = try! String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
+let lines = input.split(separator: "\n", omittingEmptySubsequences: false)
